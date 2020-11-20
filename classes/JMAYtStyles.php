fb05a68ba72f69bb28465a52364d8880989ab217
johnnya23 <johnantonacci23@gmail.com>
2020-11-20 16:44:36 -0700

    minify css

--- classes/JMAYtStyles.php
+++ classes/JMAYtStyles.php
@@ -59,6 +59,43 @@ class JMAYtStyles
         return $return;
     }
 
+    protected static function minifyCss($css)
+    {
+        // some of the following functions to minimize the css-output are directly taken
+        // from the awesome CSS JS Booster: https://github.com/Schepp/CSS-JS-Booster
+        // all credits to Christian Schaefer: http://twitter.com/derSchepp
+        // remove comments
+        $css = preg_replace('!/\*[^*]*\*+([^/][^*]*\*+)*/!', '', $css);
+        // backup values within single or double quotes
+        preg_match_all('/(\'[^\']*?\'|"[^"]*?")/ims', $css, $hit, PREG_PATTERN_ORDER);
+        for ($i=0; $i < count($hit[1]); $i++) {
+            $css = str_replace($hit[1][$i], '##########' . $i . '##########', $css);
+        }
+        // remove traling semicolon of selector's last property
+        $css = preg_replace('/;[\s\r\n\t]*?}[\s\r\n\t]*/ims', "}\r\n", $css);
+        // remove any whitespace between semicolon and property-name
+        $css = preg_replace('/;[\s\r\n\t]*?([\r\n]?[^\s\r\n\t])/ims', ';$1', $css);
+        // remove any whitespace surrounding property-colon
+        $css = preg_replace('/[\s\r\n\t]*:[\s\r\n\t]*?([^\s\r\n\t])/ims', ':$1', $css);
+        // remove any whitespace surrounding selector-comma
+        $css = preg_replace('/[\s\r\n\t]*,[\s\r\n\t]*?([^\s\r\n\t])/ims', ',$1', $css);
+        // remove any whitespace surrounding opening parenthesis
+        $css = preg_replace('/[\s\r\n\t]*{[\s\r\n\t]*?([^\s\r\n\t])/ims', '{$1', $css);
+        // remove any whitespace between numbers and units
+        $css = preg_replace('/([\d\.]+)[\s\r\n\t]+(px|em|pt|%)/ims', '$1$2', $css);
+        // shorten zero-values
+        $css = preg_replace('/([^\d\.]0)(px|em|pt|%)/ims', '$1', $css);
+        // constrain multiple whitespaces
+        $css = preg_replace('/\p{Zs}+/ims', ' ', $css);
+        // remove newlines
+        $css = str_replace(array("\r\n", "\r", "\n"), '', $css);
+        // Restore backupped values within single or double quotes
+        for ($i=0; $i < count($hit[1]); $i++) {
+            $css = str_replace('##########' . $i . '##########', $hit[1][$i], $css);
+        }
+        return $css;
+    }
+
     /**
      * function jmayt_styles add the plugin specific styles
      * @return $css the css string
@@ -295,6 +332,7 @@ class JMAYtStyles
         }
     }
     }' . $jmayt_css;
+        $css = JMAYtStyles::minifyCss($css);
         return $css;
     }
 }
